951c061c027970c8a25fe7dcbfd3b7685b7a3f7a
ADAPA MADHU PRIYA <madhupriyaadapa05@gmail.com>
2026-03-10 15:04:01 +0530

    updated

---
+++ routes/productRoute.js
@@ -0,0 +1,10 @@
+import { getProducts,addProductForm,addProduct } from "../controllers/productController.js";
+import express from "express";
+const productRouter = express.Router();
+
+productRouter.get("/", getProducts);
+productRouter.get("/add", addProductForm)
+productRouter.post("/add", addProduct)
+
+
+export default productRouter
